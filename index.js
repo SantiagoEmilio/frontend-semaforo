f38fc34e42ddb47c596b73da694bdc74bcf78dae
Santiago <slopez@scl.edu.gt>
2025-06-03 13:37:00 -0600

    actualizado HOY

--- index.js
+++
@@ -1,9 +0,0 @@
-import { crearSemaforo } from "./components/semaforo/semaforo.js";
-function carga_dom() {
-  let dom = document.querySelector("#root");
-  dom.className = "dom";
-  dom.appendChild(crearSemaforo());
-  return dom;
-}
-
-carga_dom();
